c75e5d96130abb0af2afcd777188667562be4e82
Sandor Kis <kisandor@hotmail.com>
2023-03-22 22:06:49 +0000

    first request

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -3,7 +3,20 @@
     import 'bootstrap/dist/css/bootstrap.min.css';
     // import of Nav_bar
     import Nav from './nav.svelte';
+    
+    //import book component
+    import Book from './book.svelte';
+
+
+    const bookList = new Array(10).fill(0);
 
 </script>
 
+<!--Nav bar on the top of the page-->
 <Nav />
+<!-- main container-->
+<div class="container mt-3 bg-light">
+    <div class="row align-items-center">
+        <Book />
+    </div>
+</div>
