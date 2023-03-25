d3972541e167e05279fbdbcd37fe8103d5509fd3
Sandor Kis <kisandor@hotmail.com>
2023-03-25 22:13:30 +0000

    components has been moved

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -2,13 +2,10 @@
     // import the Bootstrap style files
     import 'bootstrap/dist/css/bootstrap.min.css';
     // import of Nav_bar
-    import Nav from './nav.svelte';
+    import Nav from '../lib/components/nav.svelte';
     
     //import book component
-    import Book from './book.svelte';
-
-
-    const bookList = new Array(10).fill(0);
+    import Book from '../lib/components/book.svelte';
 
 </script>
 
