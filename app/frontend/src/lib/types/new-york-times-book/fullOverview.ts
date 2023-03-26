37731f0c64c29f72bebc9ba7f9e9f74be06ea232
Sandor Kis <kisandor@hotmail.com>
2023-03-26 22:15:40 +0100

    Book interface fixed
    properties has been renamed

--- app/frontend/src/lib/types/new-york-times-book/fullOverview.ts
+++ app/frontend/src/lib/types/new-york-times-book/fullOverview.ts
@@ -27,32 +27,36 @@ export interface List {
 }
 
 export interface Book {
-    ageGroup:           string;
-    amazonProductURL:   string;
-    articleChapterLink: string;
-    author:             string;
-    bookImage:          string;
-    bookImageWidth:     number;
-    bookImageHeight:    number;
-    bookReviewLink:     string;
-    bookURI:            string;
-    contributor:        string;
-    contributorNote:    string;
-    createdDate:        Date;
-    description:        string;
-    firstChapterLink:   string;
-    price:              string;
-    primaryIsbn10:      string;
-    primaryIsbn13:      string;
-    publisher:          string;
-    rank:               number;
-    rankLastWeek:       number;
-    sundayReviewLink:   string;
-    title:              string;
-    updatedDate:        Date;
-    weeksOnList:        number;
-    buyLinks:           BuyLink[];
+    age_group: string;
+    amazon_product_url: string;
+    article_chapter_link: string;
+    author: string;
+    book_image: string;
+    book_image_width: number;
+    book_image_height: number;
+    book_review_link: string;
+    book_uri: string;
+    contributor: string;
+    contributor_note: string;
+    created_date: string;
+    description: string;
+    first_chapter_link: string;
+    price: string;
+    primary_isbn10: string;
+    primary_isbn13: string;
+    publisher: string;
+    rank: number;
+    rank_last_week: number;
+    sunday_review_link: string;
+    title: string;
+    updated_date: string;
+    weeks_on_list: number;
+    buy_links: {
+        name: string;
+        url: string;
+    }[];
 }
+
 
 export interface BuyLink {
     name: Name;
