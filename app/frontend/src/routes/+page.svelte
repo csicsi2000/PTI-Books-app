7ea02bfc9664a10ce9d422167021212265b4eedb
Sandor Kis <kisandor@hotmail.com>
2023-03-26 22:18:15 +0100

    flatenPageData function has been created
    function created to return a single array of all the books from all the list

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,16 +1,37 @@
-
 <script lang="ts">
-    // import the Bootstrap style files
-    import 'bootstrap/dist/css/bootstrap.min.css';
-    //import booklist component
-    import Booklist from '$lib/components/booklist.svelte';
+	// import the Bootstrap style files
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	//import booklist component
+	import Booklist from '$lib/components/booklist.svelte';
+	import type { Book, List } from '$lib/types/new-york-times-book/fullOverview';
+
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	function flatenPageData(data: PageData): Book[] {
+		try {
+			/* 
+            Use the 'flatMap' function to create a single array of all the books 
+            in all the lists
+            */
+			const books = data.results.lists.flatMap((list) => list.books) as Book[];
+            console.log((books[0] as Book).book_image);
+			return books;
+
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	}
+
+	let fullBookList = flatenPageData(data);
+    
 
 </script>
 
-
 <!-- main container-->
 <div class="container mt-3 bg-light">
-    <div class="row">
-        <Booklist />
-    </div>
+	<div class="row">
+		<Booklist books={fullBookList} />
+	</div>
 </div>
