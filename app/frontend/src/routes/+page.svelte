28703a4b2365b0c7d78770cd723a57a0f8c9f699
Sandor Kis <kisandor@hotmail.com>
2023-03-26 16:48:17 +0100

    sticky nav fixed

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -2,16 +2,12 @@
 <script lang="ts">
     // import the Bootstrap style files
     import 'bootstrap/dist/css/bootstrap.min.css';
-    // import of Nav_bar
-    import Nav from '../lib/components/nav.svelte';
-    
     //import book component
     import Book from '../lib/components/book.svelte';
 
 </script>
 
-<!--Nav bar on the top of the page-->
-<Nav />
+
 <!-- main container-->
 <div class="container mt-3 bg-light">
     <div class="row">
