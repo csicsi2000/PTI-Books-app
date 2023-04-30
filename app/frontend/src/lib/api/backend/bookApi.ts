3d506259f913cd630bfdf1daf890d68c7a5c3a5d
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2023-04-30 18:51:22 +0200

    feat: add concept frontend call implementations

---
+++ app/frontend/src/lib/api/backend/bookApi.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import  type { AxiosResponse } from "axios";
+import type { Book } from 'shared-component/dist/entity/Book';
+import { BASE_URL, getToken } from "./authApi";
+
+const apiUrl = BASE_URL;
+
+export const getBook = async (bookId: string): Promise<Book> => {
+  const response: AxiosResponse<Book> = await axios.get(`${apiUrl}/books/${bookId}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return response.data;
+};
+
+export const postReview = async (
+  userId: string,
+  bookId: string,
+  comment: string,
+  rating: number
+): Promise<string> => {
+  const response: AxiosResponse<string> = await axios.post(
+    `${apiUrl}/books/${userId}/reviews`,
+    { bookId, comment, rating },
+    { headers: { Authorization: `Bearer ${getToken()}` } }
+  );
+  return response.data;
+};
+
+export const deleteReview = async (bookId:string,reviewId: string): Promise<string> => {
+  const response: AxiosResponse<string> = await axios.delete(`${apiUrl}/books/${bookId}/reviews/${reviewId}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return response.data;
+};
