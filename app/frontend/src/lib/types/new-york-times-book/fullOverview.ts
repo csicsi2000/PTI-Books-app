2f55627f4310756c50a0868e37a18eb344333b7f
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2023-05-02 19:07:19 +0200

    fix: remove dulicated object

--- app/frontend/src/lib/types/new-york-times-book/fullOverview.ts
+++ app/frontend/src/lib/types/new-york-times-book/fullOverview.ts
@@ -1,3 +1,5 @@
+import type { Book } from "shared-component/dist/entity/Book";
+
 export interface FullOverview {
     status:      string;
     copyright:   string;
@@ -24,40 +26,6 @@ export interface List {
     list_image_width:  string;
     list_image_height: string;
     books:             Book[];
-}
-
-export interface Book {
-    age_group:            string;
-    amazon_product_url:   string;
-    article_chapter_link: string;
-    author:               string;
-    book_image:           string;
-    book_image_width:     number;
-    book_image_height:    number;
-    book_review_link:     string;
-    contributor:          string;
-    contributor_note:     string;
-    created_date:         Date;
-    description:          string;
-    first_chapter_link:   string;
-    price:                string;
-    primary_isbn10:       string;
-    primary_isbn13:       string;
-    book_uri:             string;
-    publisher:            string;
-    rank:                 number;
-    rank_last_week:       number;
-    sunday_review_link:   string;
-    title:                string;
-    updated_date:         Date;
-    weeks_on_list:        number;
-    buy_links:            BuyLink[];
-}
-
-
-export interface BuyLink {
-    name: string;
-    url:  string;
 }
 
 export enum Updated {
