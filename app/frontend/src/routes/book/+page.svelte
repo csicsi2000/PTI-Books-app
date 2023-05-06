94062cf245881bd7075cd7718d4efc3f3d4f1ca2
Sandor Kis <53196528+kissandor@users.noreply.github.com>
2023-05-06 19:29:47 +0100

    feat: add review submit button to the book page

--- app/frontend/src/routes/book/+page.svelte
+++ app/frontend/src/routes/book/+page.svelte
@@ -46,10 +46,13 @@
 			<p><strong>Description:</strong> {book.description}</p>
 		</div>
 	</div>
-	<div class="form-floating mt-3	">
-		<textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+	<div class="form-floating mt-3 mb-3 col-12">
+		<textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 80px"></textarea>
 		<label for="floatingTextarea">Review</label>
 	</div>
+	<div class="col-12">
+		<button class="btn btn-danger" type="submit">Submit review</button>
+	  </div>
 </div>
 {/if}
 <!-- Modal -->
