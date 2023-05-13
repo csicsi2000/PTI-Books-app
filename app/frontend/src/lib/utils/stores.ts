f767e125ff85c78d2b4e785b9238d6d29cd71917
Sandor Kis <53196528+kissandor@users.noreply.github.com>
2023-05-13 21:25:56 +0100

    feat: add authResp to store

--- app/frontend/src/lib/utils/stores.ts
+++ app/frontend/src/lib/utils/stores.ts
@@ -1,13 +1,16 @@
 import { writable } from 'svelte/store';
 import type { User } from 'shared-component';
 import type { Book } from 'shared-component/dist/entity/Book';
+import type { AuthResponse } from '$lib/api/backend/authApi';
 
 type NullableUser = User | null;
 type NullableBook = Book | null;
-
+type NullableRespons = AuthResponse | null;
 
 export const count = writable<NullableUser>(null);
 
 export const selectedBook = writable<Book>();
 
 export const allBooks = writable<Book[]>();
+
+export const authResp = writable<NullableRespons>();
