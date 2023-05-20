18cad1bcd288e9570a71ddb9ddc001330c26d6d6
Sandor Kis <53196528+kissandor@users.noreply.github.com>
2023-05-20 22:14:24 +0100

    fefact: move submitMyreview frunction to function.ts

--- app/frontend/src/lib/components/reviewForm.svelte
+++ app/frontend/src/lib/components/reviewForm.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import type { AuthResponse } from '$lib/api/backend/authApi';
 	import { authResp, selectedBook } from '$lib/utils/stores';
-	import Rating from './rating.svelte';
 	import Review from './review.svelte';
-	import { postReview } from '$lib/api/backend/bookApi';
 	import type { Book } from 'shared-component/dist/entity/Book';
+	import { submitMyReview } from '$lib/utils/functions';
 
 	let reviewTitle: string;
 	let review: string;
-	let rating = 0;
+	let reviewRating = 0;
+
+	let bookRating = 0;
 
 	let userId: string;
 	authResp.subscribe((value: AuthResponse) => {
@@ -24,38 +25,24 @@
 		}
 	});
 
-
-	function setRating(id:number){
-		for (let index = 1; index <= id; index++){
+	function setRating(id: number) {
+		for (let index = 1; index <= id; index++) {
 			let element = document.getElementById(index.toString());
-			if(element){
-				element.style.color = "rgb(255, 102, 0)";
+			if (element) {
+				element.style.color = 'rgb(255, 102, 0)';
 			}
 		}
 
-		if(id<5) {
-			for (let index = id+1; index <= 5; index++){
-			let element = document.getElementById(index.toString());
-			if(element){
-				element.style.color = "rgb(33, 37, 41)";
+		if (id < 5) {
+			for (let index = id + 1; index <= 5; index++) {
+				let element = document.getElementById(index.toString());
+				if (element) {
+					element.style.color = 'rgb(33, 37, 41)';
+				}
 			}
-			
-		}	
 		}
-		
 
-
-		rating = id;
-	}
-
-	function submitMyReview() {
-		let reviewToPost =
-			"<h5 class='card-title'>" + reviewTitle + '</h5>' + "<p class='card-text'>" + review + '</p>';
-		postReview(userId.toString(), bookToReview, reviewToPost, rating)
-			.then((string) => {})
-			.catch((error) => {
-				alert(`Error submitting review`);
-			});
+		reviewRating = id;
 	}
 </script>
 
@@ -84,29 +71,64 @@
 		</div>
 		<spam>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<i class="fa fa-star rating" id="1" on:click={()=>{setRating(1); console.log(rating)}}/>
+			<i
+				class="fa fa-star rating"
+				id="1"
+				on:click={() => {
+					setRating(1);
+				}}
+			/>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<i class="fa fa-star rating" id="2" on:click={()=>{setRating(2); console.log(rating)}}/>
+			<i
+				class="fa fa-star rating"
+				id="2"
+				on:click={() => {
+					setRating(2);
+				}}
+			/>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<i class="fa fa-star rating" id="3" on:click={()=>{setRating(3); console.log(rating)}}/>
+			<i
+				class="fa fa-star rating"
+				id="3"
+				on:click={() => {
+					setRating(3);
+				}}
+			/>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<i class="fa fa-star rating" id="4" on:click={()=>{setRating(4); console.log(rating)}}/>
+			<i
+				class="fa fa-star rating"
+				id="4"
+				on:click={() => {
+					setRating(4);
+				}}
+			/>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<i class="fa fa-star rating" id="5" on:click={()=>{setRating(5); console.log(rating)}}/>
+			<i
+				class="fa fa-star rating"
+				id="5"
+				on:click={() => {
+					setRating(5);
+				}}
+			/>
 		</spam>
 		<div class="col-12">
-			<button class="btn btn-danger" id="reviewSubmit" type="submit" on:click={submitMyReview}
-				>Submit review</button
+			<button
+				class="btn btn-danger"
+				id="reviewSubmit"
+				type="submit"
+				on:click={() => {
+					submitMyReview(reviewTitle, review, userId, bookToReview, reviewRating);
+					reviewTitle = '';
+					review = '';
+					reviewRating = 0;
+				}}>Submit review</button
 			>
 		</div>
 	</form>
 {/if}
 
-
 <style>
-	.rating{
+	.rating {
 		cursor: pointer;
 	}
-		
-	
 </style>
--- app/frontend/src/lib/utils/functions.ts
+++ app/frontend/src/lib/utils/functions.ts
@@ -1,6 +1,8 @@
 import { login, register } from '$lib/api/backend/authApi';
 import { postReview } from '$lib/api/backend/bookApi';
 import { authResp } from '$lib/utils/stores';	
+import type { Book } from 'shared-component/dist/entity/Book';
+import type { Review } from 'shared-component/dist/entity/Review';
 
 export function loginToMyAccount(email: string, password: string ) {
     const loginRequestBody = {
@@ -36,3 +38,23 @@ export function registerMyAccount(email: string, password: string, firstName: st
             alert(`Error logging in: ${error.message}`);
         });
 };
+
+export function submitMyReview(reviewTitle: string, review: string, userId: string, bookToReview: Book, rating: number) {
+    if (reviewTitle.length < 1 || review.length < 1) {
+        alert('Wrong review!');
+    } else {
+        let reviewToPost =
+            "<h5 class='card-title'>" +
+            reviewTitle +
+            '</h5>' +
+            "<p class='card-text'>" +
+            review +
+            '</p>';
+        postReview(userId.toString(), bookToReview, reviewToPost, rating)
+            .then((string) => {})
+            .catch((error) => {
+                alert(`Error submitting review`);
+            });
+    }
+}
+
