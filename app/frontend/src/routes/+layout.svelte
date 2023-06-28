c0c49d29fa53899421630372261937784c7143df
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2023-06-28 11:40:09 +0200

    fix: typo

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 </div>
 	<div class="container">
 		<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-		  <p class="col-md-4 mb-0 text-body-secondary">© AFP 2 beadandó</p>
+		  <p class="col-md-4 mb-0 text-body-secondary">© AFP 2 Projekt</p>
 	  
 		  <a href="https://getbootstrap.com/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
 			<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
